a64ae8954b495808be391e22c9bc3f5bc60e1bcc
fhoeft <fhoeft92615@gmail.com>
2020-11-02 19:45:05 -0600

    2nd commit changed colors

--- rank.js
+++ rank.js
@@ -17,7 +17,24 @@ $(document).ready(function()
         var firstName = $("#firstName").val();
         var lastName = $("#lastName").val();
 
-        $("#greeting").text("Welcome, " + rank.val() + " " + firstName + " " + lastName)
-
+        $("#greeting").text("Welcome, " + rank.val() + " " + firstName + " " + lastName);
+        if (rank.val() === "King"){
+            $("#greeting").css("color", "blue")
+        }
+        if (rank.val() === "Queen"){
+            $("#greeting").css("color", "purple")
+        }
+        if (rank.val() === "Hand of the King"){
+            $("#greeting").css("color", "green")
+        }
+        if (rank.val() === "Master of Coin"){
+            $("#greeting").css("color", "red")
+        }
+        if (rank.val() === "Master of Whisperers"){
+            $("#greeting").css("color", "orange")
+        }
+        if (rank.val() === "Handmaiden"){
+            $("#greeting").css("color", "pink")
+        }
     }
 });
